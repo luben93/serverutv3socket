094533f2ce3438a0c7cd4f7da84d0a1b80dd981d
luben <luben93@gmail.com>
2016-01-08 17:31:36 +0100

    big stuff

--- index.js
+++ index.js
@@ -4,54 +4,64 @@ var io = require('socket.io')(http);
 var Rest = require('node-rest-client').Client;
 var crypto = require('crypto');
 var rest = new Rest();
+var chats = {};
 
 
 app.get('/:id/:other', function(req,res){
-    console.log(req.params.id);
-    console.log(req.params.other);
+    //console.log(req.params.id);
+    //console.log(req.params.other);
     res.sendFile(__dirname + '/index.html');
 });
 
 
 io.on('connection',function(socket){
-
-    console.log('a user connected');
-    //TODO login
-    
-    socket.broadcast.emit('hi');
-    
-    socket.on('disconnect',function(){
-    	console.log('user disconnect');
-    });
     socket.on('logon',function(path){
+        console.log("in io")
+        var room=path.match(/([1-9])+/g)[1]+""+path.match(/([1-9])+/g)[0];
         if(path.match(/([1-9])+/g)[0]<path.match(/([1-9])+/g)[1]){
-            socket.join(path.match(/([1-9])+/g)[0]+""+path.match(/([1-9])+/g)[1]);
-        }else{
-            socket.join(path.match(/([1-9])+/g)[1]+""+path.match(/([1-9])+/g)[0]);
+            room = path.match(/([1-9])+/g)[0]+""+path.match(/([1-9])+/g)[1]
+        }
+        if (typeof chats.room == "undefined") {
+            chats.room = io.of(room)
         }
 
+        socket.emit('logged on',"/"+room) 
+        console.log(chats.room)
+        chats.room.on('connection', function(socket){
 
-        rest.get("http://localhost:8081/rest/chat"+path, function(data,response){
-            data.list.forEach(function(msg){
-                io.emit('chat message',msg);
+            console.log('a user connected');
+            //TODO login
+            
+            socket.broadcast.emit('hi');
+            
+            socket.on('disconnect',function(){
+            	console.log('user disconnect');
             });
-        });
+            socket.on('logon room',function(path){
+                
+                rest.get("http://localhost:8081/rest/chat"+path, function(data,response){
+                    data.list.forEach(function(msg){
+                        io.emit('chat message',msg);
+                    });
+                });
+            });
+            socket.on('chat message',function(out){
+                
+                var matches = out.path.match(/([1-9])+/g);
+                console.log(matches);
+                rest.post("http://localhost:8081/rest/chat",  {
+                data: { sender: parseInt(matches[0]), recvier: parseInt(matches[1]), message: out.message },
+                headers:{"Content-Type": "application/json"} 
+                }, function(data,response) {
+                    console.log(data+response);
+                });
+                out.sender = parseInt(matches[0])
+            	io.emit('chat message',out);
+            });
+        })
     });
-    socket.on('chat message',function(out){
-        
-        var matches = out.path.match(/([1-9])+/g);
-        console.log(matches);
-        rest.post("http://localhost:8081/rest/chat",  {
-        data: { sender: parseInt(matches[0]), recvier: parseInt(matches[1]), message: out.message },
-        headers:{"Content-Type": "application/json"} 
-        }, function(data,response) {
-            console.log(data+response);
-        });
-        out.sender = parseInt(matches[0])
-    	io.emit('chat message',out);
-    });
-})
 
+});
 http.listen(3000, function(){
     console.log('magic *:3000');
 });
